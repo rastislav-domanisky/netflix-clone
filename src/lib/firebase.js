45a664b9a816689e9e64edf807b01c96af05765e
Rastislav Domaniský <63097727+rastislav-domanisky@users.noreply.github.com>
2021-04-03 21:23:32 +0200

    final v.1

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -5,14 +5,14 @@ import 'firebase/firestore';
 import 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA1luOVCuZSehMnZ769IbQ7L6ehB6zh1M0",
-  authDomain: "netflix-ff727.firebaseapp.com",
-  databaseURL: "https://netflix-ff727-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "netflix-ff727",
-  storageBucket: "netflix-ff727.appspot.com",
-  messagingSenderId: "852410708289",
-  appId: "1:852410708289:web:925dda3e417bb9f8d6f1c0",
-  measurementId: "G-WY4Y9B5PVX"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 firebase.initializeApp(firebaseConfig);
